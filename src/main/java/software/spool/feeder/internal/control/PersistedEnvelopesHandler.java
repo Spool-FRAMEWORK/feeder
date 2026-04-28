2975a8a0689684b9dd0ae32a9f04b794a00a84c4
javcastilla <javier.casno@gmail.com>
2026-04-28 23:16:32 +0100

    refactor: persisted enveloped handler now takes envelope persisted events instead of idempotency keys

--- src/main/java/software/spool/feeder/internal/control/PersistedEnvelopesHandler.java
+++ src/main/java/software/spool/feeder/internal/control/PersistedEnvelopesHandler.java
@@ -1,14 +1,14 @@
 package software.spool.feeder.internal.control;
 
 import software.spool.core.exception.SpoolException;
-import software.spool.core.model.vo.IdempotencyKey;
+import software.spool.core.model.event.EnvelopePersisted;
 import software.spool.core.port.bus.Handler;
 import software.spool.core.port.inbox.InboxEnvelopeRemover;
 import software.spool.core.utils.routing.ErrorRouter;
 
 import java.util.Collection;
 
-public class PersistedEnvelopesHandler implements Handler<Collection<IdempotencyKey>> {
+public class PersistedEnvelopesHandler implements Handler<Collection<EnvelopePersisted>> {
     private final InboxEnvelopeRemover remover;
     private final ErrorRouter errorRouter;
 
@@ -18,9 +18,9 @@ public class PersistedEnvelopesHandler implements Handler<Collection<Idempotency
     }
 
     @Override
-    public void handle(Collection<IdempotencyKey> idempotencyKeys) throws SpoolException {
+    public void handle(Collection<EnvelopePersisted> envelopePersistedEvents) throws SpoolException {
         try {
-            idempotencyKeys.forEach(remover::remove);
+            envelopePersistedEvents.stream().map(EnvelopePersisted::idempotencyKey).forEach(remover::remove);
         } catch (Exception e) {
             errorRouter.dispatch(e);
         }
